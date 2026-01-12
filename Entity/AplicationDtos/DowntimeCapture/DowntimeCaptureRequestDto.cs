ef0921315e9dc0a989f23aaf63489bcbbb890365
LUIS DAVID LUCIO <UPM23905@upm.com.mx>
2026-01-12 09:46:54 -0600

    feat(api): agrega DTO para solicitud de Captura de Tiempos Muertos
    Se introduce una estructura de datos para manejar las peticiones de registro o consulta de paros.
    
    feat(api):
    - Se añade 'DowntimeCaptureRequestDto' con soporte para rangos de fecha y filtrado múltiple por estaciones (Línea/Modelo).

---
+++ Entity/AplicationDtos/DowntimeCapture/DowntimeCaptureRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.AplicationDtos.DowntimeCapture
+{
+    public class DowntimeCaptureRequestDto
+    {
+        public DateTime StartDatetime { get; set; }
+        public DateTime EndDatetime { get; set; }
+
+        public List<ProductionStation> ProductionStations { get; set; } = new List<ProductionStation>();
+
+        public class ProductionStation
+        {
+            public Guid LineId { get; set; }
+            public Guid ModelId { get; set; }
+        }
+    }
+}
