8eb9a5f95e07d3fde3213be0647fb5042813c5eb
Luis David Work <davidluciovio@gmail.com>
2026-02-12 17:32:52 -0600

    Add new models for MaterialSupplier and PartNumberEstructure

---
+++ Entity/Models/ProductionControl/PartNumberEstructure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models.ProductionControl
+{
+    public class PartNumberEstructure
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreateBy { get; set; } = string.Empty;
+        public DateTime UpdateDate { get; set; }
+        public string UpdateBy { get; set; } = string.Empty;
+
+        public Guid PartNumberLogisticId { get; set; }
+        public virtual PartNumberLogistics PartNumberLogistics { get; set; } = new PartNumberLogistics();
+        public Guid ComponentId { get; set; }
+        public string ComponentName { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public Guid MaterialSuplierId { get; set; }
+        public virtual MaterialSupplier MaterialSupplier { get; set; } = new MaterialSupplier();
+    }
+}
